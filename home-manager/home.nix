0f1fade934633d8b31985f69e1163286f7b3a228
ph0ryn <208665755+ph0ryn@users.noreply.github.com>
2026-04-06 23:44:55 +0900

    home: add git

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,6 +19,7 @@
   # environment.
   home.packages = with pkgs; [
     nixfmt
+    git
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
