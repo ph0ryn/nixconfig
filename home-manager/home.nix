0ac75a724170854ff6ad216653a1efefcfe21520
ph0ryn <208665755+ph0ryn@users.noreply.github.com>
2026-04-06 23:57:55 +0900

    home: add pnpm

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -20,6 +20,7 @@
   home.packages = with pkgs; [
     nixfmt
     git
+    pnpm
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
