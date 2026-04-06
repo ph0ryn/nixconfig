6c124582d5deb7b125295a08a0cf730d3242a8e0
ph0ryn <208665755+ph0ryn@users.noreply.github.com>
2026-04-07 00:47:26 +0900

    chore: allow unfree

--- flake.nix
+++ flake.nix
@@ -22,7 +22,10 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home-manager/home.nix ];
+        modules = [
+          ./configuration.nix
+          ./home-manager/home.nix
+        ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
