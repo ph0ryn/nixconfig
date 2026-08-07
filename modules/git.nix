d7e3959e26cc0116c0eb9d16ee8eba60d4d9999d
ph0ryn <208665755+ph0ryn@users.noreply.github.com>
2026-08-07 21:07:49 +0900

    feat(git): manage Git configuration through Home Manager

--- modules/git.nix
+++ modules/git.nix
@@ -1,48 +1,65 @@
 { pkgs, ... }:
 {
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        init.defaultBranch = "main";
+        push.followTags = true;
+      };
+      ignores = [
+        ".DS_Store"
+
+        ".vscode/launch.json"
+
+        ".ruff_cache/"
+        ".ty_cache/"
+        ".venv/"
+        ".indexion/"
+        ".rumdl_cache/"
+
+        "node_modules/"
+      ];
+    };
+    gh = {
+      enable = true;
+      extensions = [
+        pkgs.gh-fzf-get
+        pkgs.gh-license
+      ];
+      settings = {
+        version = 1;
+        git_protocol = "ssh";
+        prompt = "enabled";
+        prefer_editor_prompt = "disabled";
+        aliases = {
+          co = "pr checkout";
+        };
+        color_labels = "enabled";
+        accessible_colors = "disabled";
+        accessible_prompter = "disabled";
+        spinner = "enabled";
+      };
+    };
+
+    lazygit = {
+      enable = true;
+      settings = {
+        gui = {
+          language = "en";
+        };
+        git = {
+          autoFetch = true;
+          autoFetchInterval = 120;
+        };
+      };
+    };
+  };
+
   home.packages = with pkgs; [
     ghq
-    git
     git-filter-repo
     prek
     keifu
   ];
-
-  programs.lazygit = {
-    enable = true;
-    settings = {
-      gui = {
-        language = "en";
-      };
-      git = {
-        autoFetch = true;
-        autoFetchInterval = 120;
-      };
-    };
-  };
-
-  programs.gh = {
-    enable = true;
-
-    extensions = [
-      pkgs.gh-fzf-get
-      pkgs.gh-license
-    ];
-
-    settings = {
-      version = 1;
-      git_protocol = "ssh";
-      prompt = "enabled";
-      prefer_editor_prompt = "disabled";
-
-      aliases = {
-        co = "pr checkout";
-      };
-
-      color_labels = "enabled";
-      accessible_colors = "disabled";
-      accessible_prompter = "disabled";
-      spinner = "enabled";
-    };
-  };
 }
