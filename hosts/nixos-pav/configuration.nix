94640dcbb58f6e35957190a5168fa2d05e2d7169
ph0ryn <208665755+ph0ryn@users.noreply.github.com>
2026-08-14 21:06:46 +0900

    feat(nixos): configure Japanese input on Pavilion

--- hosts/nixos-pav/configuration.nix
+++ hosts/nixos-pav/configuration.nix
@@ -28,21 +28,6 @@
 
   time.timeZone = "Asia/Tokyo";
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "ja_JP.UTF-8";
-      LC_IDENTIFICATION = "ja_JP.UTF-8";
-      LC_MEASUREMENT = "ja_JP.UTF-8";
-      LC_MONETARY = "ja_JP.UTF-8";
-      LC_NAME = "ja_JP.UTF-8";
-      LC_NUMERIC = "ja_JP.UTF-8";
-      LC_PAPER = "ja_JP.UTF-8";
-      LC_TELEPHONE = "ja_JP.UTF-8";
-      LC_TIME = "ja_JP.UTF-8";
-    };
-  };
-
   hardware.bluetooth.enable = true;
   security.rtkit.enable = true;
 
@@ -79,5 +64,6 @@
     ./hardware-configuration.nix
     ./system.nix
     ./service.nix
+    ./language.nix
   ];
 }
---
+++ hosts/nixos-pav/language.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+{
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "ja_JP.UTF-8";
+      LC_IDENTIFICATION = "ja_JP.UTF-8";
+      LC_MEASUREMENT = "ja_JP.UTF-8";
+      LC_MONETARY = "ja_JP.UTF-8";
+      LC_NAME = "ja_JP.UTF-8";
+      LC_NUMERIC = "ja_JP.UTF-8";
+      LC_PAPER = "ja_JP.UTF-8";
+      LC_TELEPHONE = "ja_JP.UTF-8";
+      LC_TIME = "ja_JP.UTF-8";
+    };
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-mozc
+        ];
+        waylandFrontend = true;
+      };
+    };
+  };
+}
